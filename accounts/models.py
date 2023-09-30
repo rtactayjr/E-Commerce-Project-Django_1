8881e30425ba9b4918bea43289b1c1aa4ef67452
r-tactay <rolandtactay571@gmail.com>
2023-09-30 12:40:31 +0800

    create accounts app, update database to postgresql

---
+++ accounts/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# Create your models here.
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, phone_number, password=None):
+        
+        if not email:
+            raise ValueError('The Email field must be set')
+        
+        if not username:
+            raise ValueError('Username must be provided')
+        
+        email = self.normalize_email(email) # Normalize email
+        
+        user = self.model(
+            email=email, 
+            username=username, 
+            first_name=first_name, 
+            last_name=last_name, 
+            phone_number=phone_number
+                          )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, phone_number, password=None):
+        email = self.normalize_email(email)  # Normalize email
+
+        user = self.create_user(
+            email=email, 
+            username=username, 
+            first_name=first_name, 
+            last_name=last_name, 
+            hone_number=phone_number, 
+            password=password
+            )
+        
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.is_superadmin = True
+        
+        user.save(using=self._db)
+        return user
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+    # required fields
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    # flags
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    objects = MyAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, add_label):
+        return True
--- fashionflow/settings.py
+++ fashionflow/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -71,13 +72,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'fashionflow.wsgi.application'
 
 
+# Sys User Model
+# Use Custom User Model for authentication
+AUTH_USER_MODEL = 'accounts.Account'
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FashionFlow',
+        'USER': 'postgres',
+        'PASSWORD':'postgres2023',
+        'HOST': 'localhost',
     }
 }
 
