3463c211d363f922965cbdd83ebec57a5487f3e1
r-tactay <rolandtactay571@gmail.com>
2023-10-02 14:57:17 +0800

    add function to decrease cart quantity and delete a cart item

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 
@@ -39,6 +39,26 @@ def add_to_cart(request, product_id):
             )
         cart_item.save()
     return redirect('cart')
+
+def remove_from_cart(request,product_id,):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product =  get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product =  get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
             
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
