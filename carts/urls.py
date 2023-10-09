c1831cc355e618ea5b530396594fd3579e93a9aa
r-tactay <rolandtactay571@gmail.com>
2023-10-09 13:00:24 +0800

    Update cart functionality for decrement and increment

--- carts/urls.py
+++ carts/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_to_cart/<int:product_id>', views.add_to_cart, name='add_to_cart'),
     
-    path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
+    path('remove_from_cart/<int:product_id>/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item'),
 
     # path('checkout_cart/',views.checkout_cart, name='checkout_cart'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -86,22 +86,27 @@ def add_to_cart(request, product_id):
         
     return redirect('cart')
 
-def remove_from_cart(request,product_id,):
+def remove_from_cart(request,product_id, cart_item_id ):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product =  get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
-
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+            
+    except:
+        pass
+    
     return redirect('cart')
 
-def remove_cart_item(request, product_id):
+
+def remove_cart_item(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product =  get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
