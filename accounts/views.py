3557cc15090edb5cb4fb6561dc5d761bfbe81094
r-tactay <rolandtactay571@gmail.com>
2023-10-10 23:38:01 +0800

    Create forgot password function

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages,auth
@@ -93,6 +94,51 @@ def activate(request,uidb64, token):
         messages.error(request, 'Invalid activation link')
         return redirect('register')
     
+    
 @login_required(login_url='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+
+def forgotPassword(request):
+    if request.method =='POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user =  Account.objects.get(email__iexact=email)
+
+            #Reset Password Email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Your Password'
+            message = render_to_string('accounts/reset_password_email.html',{
+                'user':user,
+                'domain': current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been send to your email address')
+            return redirect('login')
+        
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgotPassword')
+        
+    return render(request, 'accounts/forgot_password.html')
+
+def resetpassword_validate(request, uidb64, token):
+    # try:
+    #     uid = urlsafe_base64_decode(uidb64).decode()
+    #     user = Account._default_manager.get(pk=uid)
+    # except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+    #     user = None
+    
+    # if user is not None and default_token_generator.check_token(user, token):
+    #     request.session['uid'] = uid
+    #     messages.success(request,'Please reset your password')
+    #     return redirect('resetPassword')
+    # else:
+    #     messages.error(request,'This link has been expired. Please try again')
+    #     return redirect('login')
+    return HttpResponse('ok')
