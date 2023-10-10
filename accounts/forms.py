2d93b3de7ca491fe929904b9e658dee0270f8fa6
r-tactay <rolandtactay571@gmail.com>
2023-10-10 11:00:35 +0800

    Update register function

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,15 +15,15 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
 
-    # def clean(self):
-    #     cleaned_data = super(RegistrationForm, self).clean()
-    #     password = cleaned_data.get('password')
-    #     confirm_password = cleaned_data.get('confirm_password')
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
 
-    #     if password != confirm_password:
-    #         raise forms.ValidationError(
-    #             "Passwords do not match."
-    #         )
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Passwords do not match."
+            )
     
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
