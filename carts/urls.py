c0e832b7571bcdf6b68cee39a388dd823e59a8ee
r-tactay <rolandtactay571@gmail.com>
2023-10-11 15:59:35 +0800

    Add checkout design

--- carts/urls.py
+++ carts/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('remove_from_cart/<int:product_id>/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item'),
 
-    # path('checkout_cart/',views.checkout_cart, name='checkout_cart'),
+    path('checkout_cart/',views.checkout_cart, name='checkout_cart'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -140,5 +140,28 @@ def cart(request, total=0, quantity=0, cart_items=None):
     
     return render(request, 'carts/cart.html', context)
 
-# def checkout_cart(request):
-#     return render(request, 'carts/checkout_cart.html')
+def checkout_cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+            
+        tax = (2 * total) /100
+        grand_total = total + tax
+
+    except ObjectDoesNotExist:
+        pass #ignore this
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'carts/checkout_cart.html', context )
