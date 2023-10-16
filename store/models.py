65e17139fd0c6b67aff4110cbfb4a5266dac2bc5
r-tactay <rolandtactay571@gmail.com>
2023-10-16 17:45:41 +0800

    Setting up review and rating system

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from products . models import Product
+from accounts . models import Account
 # Create your models here.
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
